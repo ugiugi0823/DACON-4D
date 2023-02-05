02f931aeac7fece4e1edc23a7c889d26c4c31815
Hyunwook Jo <acerghjk@gmail.com>
2023-02-05 14:39:17 +0900

    Update imageprocess.py

--- utils/imageprocess.py
+++ utils/imageprocess.py
@@ -1,51 +1,10 @@
-import os
-import csv
-import cv2
-import argparse
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-from PIL import Image
-from sklearn.model_selection import KFold
-import random
-from time import time
-import IPython
-import copy
-
-import torch
-import torch.optim as optim
-from torch import nn
-from torch.utils.data import Dataset, DataLoader
-from torchvision import transforms
-
-from warmup_scheduler import GradualWarmupScheduler
-
-import torchvision.transforms.functional as TF
-# from src.train import train_model
-# from utils.imageprocess import image_transformer, image_processor
-# from utils.EarlyStopping import EarlyStopping
-# from utils.dataloader import CustomDataLoader
-# from utils.radams import RAdam
-# from utils.call_model import CallModel
-
-import albumentations as A
-from albumentations.pytorch.transforms import ToTensorV2
-
-from tqdm import tqdm
-import logging
-
 # imageprocess.py
 
-class RotateTransform:
-    def __init__(self, angles):
-        self.angles = angles
+import random
+from torchvision import transforms
+import torchvision.transforms.functional as TF
 
-    def __call__(self, x):
-        if isinstance(self.angles, list):
-            angle = random.choice(self.angles)
-        else:
-            angle = self.angles
-        return TF.rotate(x, angle)
+
 
 
 def image_transformer(input_image=None, train=True):
@@ -70,25 +29,6 @@ def image_transformer(input_image=None, train=True):
             transforms.ToTensor(),
             normalize,
         ])
-
-    transformed_image = transformer(input_image)
-    
-    return transformed_image
-
-
-
-def tta_transformer(input_image, angle):
-    """
-    Test Time Augmentation for creating final test labels.
-    """
-    normalize = transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
-
-    transformer = transforms.Compose([       
-        transforms.Resize((224, 224)), 
-        RotateTransform(angle),
-        transforms.ToTensor(),
-        normalize,
-    ])
 
     transformed_image = transformer(input_image)
     
